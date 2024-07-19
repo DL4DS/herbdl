55ad058d5aa65b71f25ad874eafb84d2e9a4c663
Farid-Karimli <karimli.farid@icloud.com>
2024-07-19 19:27:47 -0400

    Removing API keys

--- utils/image_install.sh
+++ utils/image_install.sh
@@ -5,9 +5,6 @@ module load academic-ml/spring-2024
 
 conda activate spring-2024-pyt
 
-export PUSHOVER_API_TOKEN=a56w88zdckxutyn1kvgjae4mtn94cj
-export PUSHOVER_USER_KEY=um1aa9q3cyj4qcyb3johyu3i3es8d9
-
 python image_install_parallel.py
 
 ### The command below is used to submit the job to the cluster
--- utils/notifications.py
+++ utils/notifications.py
@@ -1,10 +1,21 @@
 import requests
 import json
 import os
+from dotenv import load_dotenv
 
-URL = "https://api.pushover.net/1/messages.json"
+load_dotenv()
 
 def send_notification(title, message):
+
+    URL = "https://api.pushover.net/1/messages.json"
+
+    api_token = os.getenv("PUSHOVER_API_TOKEN")
+    user_key = os.getenv("PUSHOVER_USER_KEY")
+
+    if not api_token or not user_key:
+        print("Pushover API token or user key not set")
+        return
+
     data = {
         "token": os.getenv("PUSHOVER_API_TOKEN"),
         "user": os.getenv("PUSHOVER_USER_KEY"),
@@ -12,6 +23,3 @@ def send_notification(title, message):
         "message": message
     }
     requests.post(URL, data=data)
-
-
-# send_notification("Test", "This is a test message")
