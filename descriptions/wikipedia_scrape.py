a9bde3e1af316ab993f380e2bba4c0e38d4c5721
Farid-Karimli <karimli.farid@icloud.com>
2025-02-05 10:50:07 -0500

    Tweak

--- descriptions/wikipedia_scrape.py
+++ descriptions/wikipedia_scrape.py
@@ -6,11 +6,14 @@ import json
 from constants import KAGGLE_HERBARIUM_22_TRAIN_CSV
 
 df = pd.read_csv(KAGGLE_HERBARIUM_22_TRAIN_CSV)
-print(df.columns)
 
-output_file = "wiki_descriptions.json"
+# Take top 10 species by frequency
+df = df.groupby(["genus", "species"]).size().reset_index(name="count")
+df = df.sort_values(by="count", ascending=False)
+df = df.head(15)
 
 labels = (df["genus"] + " " + df["species"]).unique().tolist()
+
 print(f"Number of unique species (labels): {len(labels)}")
 print(f"Labels: {labels[:5]}")
 
@@ -39,15 +42,7 @@ for sp in tqdm(labels):
 
         n_found += 1
 
-    if n_found % 100 == 0 and n_found > 0:
-        print(f"Number of species descriptions found: {n_found}")
-
 print(f"Number of species descriptions found: {len(species_found)}")
 
-
-json.dump(species_found, open("species_found.json", "w"))
-
-first = species_found[0]
-print(f"Species: {first['species']}")
-print(f"Description: {first['description'][:200]}")
-print(f"Length: {len(first['description'])}")
+output_file = "Top15_wiki_desc.json"
+json.dump(species_found, open(output_file, "w"))
